a09a1294648b7e568155b8a06981b1af521edc1f
ArtiomRamazanovgit config --global user.email tiomas15@gmail.comgit config --global --listgit --versiongit config --global user.name ArtiomRamazanov <tiomas15@gmail.com>
2025-04-28 09:57:26 +0300

    RECT CIRCLE TRIANGEL

--- script.js
+++ script.js
@@ -23,6 +23,33 @@ window.addEventListener("load", ()=> {
     setCanvasBackground();
 })
 //shapes
+
+
+const drawRect = (e) => {
+    if (!fillColor.checked) {
+        return ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
+    }
+    ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
+};
+
+const drawCircle = (e) => {
+    ctx.beginPath();
+    let radius = Math.sqrt(Math.pow(prevMouseX - e.offsetX, 2) + Math.pow(prevMouseY - e.offsetY, 2));
+    ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI);
+    fillColor.checked ? ctx.fill() : ctx.stroke();
+};
+
+const drawTriangle = (e) => {
+    ctx.beginPath();
+    ctx.moveTo(prevMouseX, prevMouseY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY);
+    ctx.closePath();
+    fillColor.checked ? ctx.fill() : ctx.stroke();
+};
+
+
+
 const startDraw = (e) => {
     isDrawing = true;
     prevMouseX = e.offsetX;
@@ -34,18 +61,26 @@ const startDraw = (e) => {
     snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 }
 
-const drawing = (e) =>{
-    if(!isDrawing) return;
+const drawing = (e) => {
+    if (!isDrawing) return;
     ctx.putImageData(snapshot, 0, 0);
 
-if(selectedTool === "brush" || selectedTool ==="easer")
-{
-    ctx.strokeStyle = selectedTool === "easer" ? "#fff" : selectedColor;
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
-}   ///kitos formos.
+    if (selectedTool === "brush" || selectedTool === "easer") {
+        ctx.strokeStyle = selectedTool === "easer" ? "#fff" : selectedColor;
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
 
+    } else if (selectedTool === "rectangle") {
+        drawRect(e);
+
+    } else if (selectedTool === "circle") { 
+        drawCircle(e);
+    
+} else if (selectedTool === "triangle") { 
+    drawTriangle(e); 
 }
+};
+
 toolBtns.forEach(btn => {
 
     btn.addEventListener("click", () => {
@@ -75,6 +110,13 @@ clearCanvas.addEventListener("click", () =>{
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     setCanvasBackground();
 })
+
+saveImg.addEventListener("click", () =>{
+    const link = document.createElement("a");
+    link.download = `${Date.now()}.jpg`;
+    link.href = canvas.toDataURL();
+    link.click();
+});
 
 
 canvas.addEventListener("mousedown", startDraw);
